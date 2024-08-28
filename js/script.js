6e7cec2a8a9dd99bb5b06ec2debb8c01b362095c
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-28 17:04:51 -0400

    Team

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ MILESTONE 1:
 Stampare su console le informazioni di nome, ruolo e la stringa della foto
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe.*/
+
+
+//TODO ELABORAZIONE
+//Creare un array del team
+Const team = [
+    {name:'Wayne Barnett', Profession:'Founder & CEO' , Photo:'wayne-barnett-founder-ceo.jpg'},
+    {name:'Angela Caroll', Profession:'Chief Editor' , Photo:'angela-caroll-chief-editor.jpg'},
+    {name:'Walter Gordon', Profession:'Office Manager', Photo:'Office Manager'},
+    {name:'Angela Lopez', Profession:'Social Media Manager', Photo:'angela-lopez-social-media-manager.jpg'},
+    {name:'Scott Estrada', Profession:'Developer', Photo:'scott-estrada-developer.jpg'},
+    {name:'Barbara Ramos', Profession:'Graphic Designer', Photo:'barbara-ramos-graphic-designer.jpg'},
+]
+//Stampare su Console
+
+//TODO OUTPUT
